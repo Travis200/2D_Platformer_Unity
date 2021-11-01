34ed0f1645eb8695414636a5b3baf8fb3edabc43
Travis Higgins <75436407+Travis200@users.noreply.github.com>
2021-11-01 15:16:56 +0000

    Enemy now patrols from wall to wall inside arena and flips and changes travel direction when contact with wall is made

--- Games_Coursework/Assets/Scripts/SquishableEnemyMovement.cs
+++ Games_Coursework/Assets/Scripts/SquishableEnemyMovement.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SquishableEnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float EnemyMovementSpeed =  4;
+    private bool moveRight = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            moveRight = !moveRight;
+            Vector3 enemyScale = transform.localScale;
+            enemyScale.x = -1;
+            transform.localScale = enemyScale;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+
+        if (moveRight)
+        {
+            transform.Translate(Vector2.right * EnemyMovementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * EnemyMovementSpeed * Time.deltaTime);
+        }
     }
 }
