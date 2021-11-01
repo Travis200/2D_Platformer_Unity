b5c88968bfe5b1e034ab3352fbe45f536852116c
Travis Higgins <75436407+Travis200@users.noreply.github.com>
2021-11-01 17:34:50 +0000

    Add death zone for when player falls off of map

--- Games_Coursework/Assets/Scripts/PlayerDeath.cs
+++ Games_Coursework/Assets/Scripts/PlayerDeath.cs
@@ -6,11 +6,10 @@ public class PlayerDeath : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("DeathZone"))
         {
             Destroy(gameObject);
             LevelController.instance.Respawn();
-/*            FindObjectOfType<LevelController>().Player*/
         }
     }
 }
