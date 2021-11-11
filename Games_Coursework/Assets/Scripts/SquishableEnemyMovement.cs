314e2d79c4608641ef9c05dedf625f296bfe0bff
Travis Higgins <75436407+Travis200@users.noreply.github.com>
2021-11-11 17:47:52 +0000

    Added stochastic behaviour to squishable enemies (direction, speed etc)

--- Games_Coursework/Assets/Scripts/SquishableEnemyMovement.cs
+++ Games_Coursework/Assets/Scripts/SquishableEnemyMovement.cs
@@ -3,21 +3,28 @@ using UnityEngine;
 public class SquishableEnemyMovement : MonoBehaviour
 {
 
-    public float EnemyMovementSpeed =  4;
+    public float MaxEnemyMovementSpeed =  5f;
+    public float MinEnemyMovementSpeed = 2.5f;
+    private float EnemyMovementSpeed;
     private bool moveRight = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if ((collision.gameObject.CompareTag("Wall")) || collision.gameObject.CompareTag("Enemy"))
         {
-            moveRight = !moveRight;
-            Vector3 enemyScale = transform.localScale;
-            enemyScale.x *= -1;
-            transform.localScale = enemyScale;
+            FlipEnemy();
         }
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        // This ensures that different squishable enemies move at different speeds to help implement some stochastic behaviour into the game
+        EnemyMovementSpeed = Random.Range(MinEnemyMovementSpeed, MaxEnemyMovementSpeed);
+        // This makes the enemies change direction at random times to make the games less predictable and helps with the stochastic model
+        InvokeRepeating("FlipEnemy", 5f, Random.Range(3f, 10f));
+        InvokeRepeating("ChangeEnemySpeed", 5f, Random.Range(3f, 15f));
+    }
+
     private void Update()
     {
         Patrol();
@@ -35,4 +42,18 @@ public class SquishableEnemyMovement : MonoBehaviour
             transform.Translate(Vector2.left * EnemyMovementSpeed * Time.deltaTime);
         }
     }
+
+    private void FlipEnemy()
+    {
+        moveRight = !moveRight;
+        Vector3 enemyScale = transform.localScale;
+        enemyScale.x *= -1;
+        transform.localScale = enemyScale;
+    }
+
+    private void ChangeEnemySpeed()
+    {
+        EnemyMovementSpeed = Random.Range(MinEnemyMovementSpeed, MaxEnemyMovementSpeed);
+    }
+
 }
